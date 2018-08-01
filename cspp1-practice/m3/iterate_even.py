6fbf031a7f662eb5514457b9bf68ed1d9d761867
manojbandari <bandarimanoj22@msitprogram.net>
2018-08-01 22:09:08 +0530

    updated even numbers 1 to n using while

--- cspp1-practice/m3/iterate_even.py
+++ cspp1-practice/m3/iterate_even.py
@@ -2,8 +2,14 @@
 @author : manojbandari
 print the even variables upto 10 and print goodbye
 '''
-N = 2
-while N <= 10:
+#using while loop
+#N = 2
+#while N <= 10:
+#   print("print", N)
+#   N += 2
+#print("print Goodbye!")
+#using for loop
+
+for N in range(2, 11, 2):
     print("print", N)
-    N += 2
 print("print Goodbye!")
