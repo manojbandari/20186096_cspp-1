ee92c63ba6a724d6a78ceff3eede229725bbc266
manojbandari <bandarimanoj22@msitprogram.net>
2018-08-01 22:31:29 +0530

    updated even numbers 1 to n in reverse order

--- cspp1-practice/m3/iterate_even_reverse.py
+++ cspp1-practice/m3/iterate_even_reverse.py
@@ -2,8 +2,14 @@
 @author : manojbandari
 Print the hello and even numbers upto 10 in reverse order
 '''
-print("print Hello!")
-N = 10
-while N >= 2:
-    print("print", N)
-    N -= 2
+#using while loop
+#print("print Hello!")
+#N = 10
+#while N >= 2:
+ #   print("print", N)
+  #  N -= 2
+
+#using for loop
+print("Hello!")
+for N in range(10, 1, -2):
+    print(N)
