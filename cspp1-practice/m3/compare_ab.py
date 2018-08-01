5d01578072ca065d8f8f0e1eda7ad6c0a826ed69
manojbandari <bandarimanoj22@msitprogram.net>
2018-08-01 19:52:49 +0530

    program using conditional statements

--- cspp1-practice/m3/compare_ab.py
+++ cspp1-practice/m3/compare_ab.py
@@ -5,7 +5,7 @@ compare the two variables and display the output based on the condtions
 VARA = input()
 VARB = input()
 if(isinstance(VARA, str) or isinstance(VARB, str)):
-    print("string involved")
+    print("stirng involved")
 elif VARA > VARB:
     print("bigger")
 elif VARA == VARB:
