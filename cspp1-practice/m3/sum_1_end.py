a2e42e058cbc57db84c0a53be8d072fe31afebda
manojbandari <bandarimanoj22@msitprogram.net>
2018-08-01 22:41:02 +0530

    updated sum of numbers 1 to n using for loop

--- cspp1-practice/m3/sum_1_end.py
+++ cspp1-practice/m3/sum_1_end.py
@@ -2,11 +2,18 @@
 @author : manojbandari
 program to print sum of 1 to n integers
 '''
+#using while loop
+# N = int(input())
+# I = 1
+# SUM = 0
+# while I <= N:
+#   SUM += I
+#  I += 1
+# print(SUM)
 
-N = int(input())
-I = 1
+#using for loop
 SUM = 0
-while I <= N:
+N = int(input())
+for I in range(1, N + 1):
     SUM += I
-    I += 1
 print(SUM)
