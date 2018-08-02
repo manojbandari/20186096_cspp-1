6bf71f4eeb162c6c634248d2a90f9d33ccfac20c
manojbandari <bandarimanoj22@msitprogram.net>
2018-08-02 23:02:23 +0530

    " -> 0 of 5 passed. pylint: 0/0 "

--- cspp1-assignments/m4/p3/p3/longest_substring.py
+++ cspp1-assignments/m4/p3/p3/longest_substring.py
@@ -18,22 +18,20 @@ If you've spent more than a few hours on this problem,
 we suggest that you move on to a different part of the course.
 If you have time, come back to this problem after you've had a break and cleared your head.
 '''
-
 def main():
-	'''
-	# the input string is in s
-	# remove pass and start your code here
-	'''
-	input_str = input()
-	temp_str=""
-	for i in range(len(input_str)-1):
-		sub_str = input_str[i]
-		while i+1 < len(input_str) and input_str[i] <= input_str[i+1]:
-			i += 1
-			sub_str += input_str[i]
-		if len(sub_str) > len(temp_str):
-			temp_str = sub_str
-	print(temp_str)
-
+    '''
+    # the input string is in s
+    # remove pass and start your code here
+    '''
+    input_str = input()
+    temp_str ="" 
+        for i in range(len(input_str)-1):
+        sub_str = input_str[i]
+        while i + 1 < len(input_str) and input_str[i] <= input_str[i+1]:
+            i += 1
+            sub_str += input_str[i]
+        if len(sub_str) > len(temp_str):
+            temp_str = sub_str
+    print(temp_str)
 if __name__== "__main__":
-	main()
+    main()
