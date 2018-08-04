d4b084a7a444c88f7033734af5f77ca2b9e0a037
manojbandari <bandarimanoj22@msitprogram.net>
2018-08-04 19:30:37 +0530

    guess the number

--- cspp1-practice/m5/GuessMyNumber/GuessMyNumber/guess_my_number.py
+++ cspp1-practice/m5/GuessMyNumber/GuessMyNumber/guess_my_number.py
@@ -1,8 +1,25 @@
+'''
+@author : manojbandari
 #Guess My Number Exercise
-
+'''
 def main():
-	s = raw_input()
-	#your code here
+    '''
+    guess the number
+    '''
+    low_num = 0
+    high_num = 100
+    print("Please think of a number between 0 and 100!")
+    print("Enter 'yes' if guess done, or 'h' if that (number > your guess ) else 'l' ")
+    user_guess = 'low_num'
+    while user_guess != 'yes':
+        print("Is your secret number ", int((low_num+high_num)//2), "?")
+        user_guess = input()
+        if user_guess == 'l':
+            low_num = ((low_num+high_num)//2)
+        elif user_guess == 'h':
+            high_num = ((low_num+high_num)//2)
+    print(user_guess)
 
-if __name__== "__main__":
-	main()
+
+if __name__ == "__main__":
+    main()
