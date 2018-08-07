e6b0e2c61e43642e82fd46ace8a8effce0e51fe1
manojbandari <bandarimanoj22@msitprogram.net>
2018-08-07 18:18:12 +0530

    " -> 10 of 10 passed. pylint: 7.78/10 "

---
+++ cspp1-assignments/m8/p1/p1/assignment1.py
@@ -0,0 +1,31 @@
+'''
+@author : manojbandari
+# Exercise: Assignment-1
+# Write a Python function, factorial(n), that takes in one number
+ and returns the factorial of given number.
+
+# This function takes in one number and returns one number.
+'''
+#from sis import *
+#sys.setrecursionlimit(10000)
+def factorial(fact_n):
+    '''
+    n is positive Integer
+
+    returns: a positive integer, the factorial of n.
+    '''
+    # Your code here
+   
+    if fact_n == 0 or fact_n == 1:
+        return 1
+    return fact_n*factorial(fact_n-1)
+
+def main():
+    '''
+    main function
+    '''
+    a_input = input()
+    print(factorial(int(a_input)))
+
+if __name__ == "__main__":
+    main()
--- cspp1-practice/m8/GCD using Iteration/GCD using Iteration/gcd_iter.py
+++ cspp1-practice/m8/GCD using Iteration/GCD using Iteration/gcd_iter.py
@@ -12,14 +12,14 @@ def gcdIter(a, b):
     '''
     # Your code here
     if a==0 and b==0
-    	return "not defined"
+        return "not defined"
     if a==0 or b==0:
-    	return 0
+        return 0
     i=1
     while(i<=a and i<=b):
-    	if a%i==0 and b%i==0:
-    		gcd = i
-    	i += 1
+        if a%i==0 and b%i==0:
+            gcd = i
+        i += 1
     return gcd
 
 def main():
