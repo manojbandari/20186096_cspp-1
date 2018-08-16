a64493e17937e4cade127002b900ee09a6b3b11d
manojbandari <bandarimanoj22@msitprogram.net>
2018-08-16 10:15:41 +0530

    " -> 15 of 19 passed. pylint: 7.71/10 "

--- cspp1-assignments/m15/CodeCampPoker/CodeCampPoker/poker.py
+++ cspp1-assignments/m15/CodeCampPoker/CodeCampPoker/poker.py
@@ -5,7 +5,7 @@ author : manojbandari
     https://en.wikipedia.org/wiki/List_of_poker_hands
 '''
 
-DIC_NEW = {'2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8,
+dic_new = {'2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8,
            '9':9, 'T':10, 'J':11, 'Q':12, 'K':13, 'A':14}
 def is_straight(hand):
     '''
