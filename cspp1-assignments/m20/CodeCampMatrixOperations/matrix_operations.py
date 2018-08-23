4017be843bfdcb276828ee2d6b76305382575abb
manojbandari <bandarimanoj22@msitprogram.net>
2018-08-23 17:47:43 +0530

    " -> 1 of 8 passed. pylint: -0.82/10 "

--- cspp1-assignments/m20/CodeCampMatrixOperations/matrix_operations.py
+++ cspp1-assignments/m20/CodeCampMatrixOperations/matrix_operations.py
@@ -39,14 +39,14 @@ def mult_matrix(m1, m2):
                         
                     mul_1m.append(a)
                     #print(mul_1m)
-                if len(m1[0])<=3:
+                if len(m1[0])==3:
                     a=0
                     count+=1
                     for n in range(len(m2)):
                         a+=int(m1[i][n])*int(m2[n][2])
                        
                     mul_1m.append(a)
-                if len(m1[0])<=4:
+                if len(m1[0])==4:
                     a=0
                     for m in range(len(m2)):
                         a+=int(m1[i][m])*int(m2[m][3])
