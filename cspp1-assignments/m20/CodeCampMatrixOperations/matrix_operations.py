30493576ba744199dd38cd91136358ade1808853
manojbandari <bandarimanoj22@msitprogram.net>
2018-08-23 19:02:46 +0530

    " -> 0 of 8 passed. pylint: -0.77/10 "

--- cspp1-assignments/m20/CodeCampMatrixOperations/matrix_operations.py
+++ cspp1-assignments/m20/CodeCampMatrixOperations/matrix_operations.py
@@ -1,20 +1,20 @@
 
 # def loop(count):
 #     a=0
-#     for j in range(len(m2)):
-#         a+=int(m1[i][j])*int(m2[j][0]) 
+#     for j in range(len(m2_matrix)):
+#         a+=int(m1_matrix[i][j])*int(m2_matrix[j][0]) 
 #             mul_1m.append(a)
-def loop(mul_1m,i,m1,m2,count):
-    count+=1
-    if len(m2[0])>=count:
-        a=0
-        j=0
-        for j in range(len(m2)):
-            a+=int(m1[i][j])*int(m2[j][count-1]) 
-        mul_1m.append(a)
+# def loop(mul_1m,i,m1_matrix,m2_matrix,count):
+#     count+=1
+#     if len(m2_matrix[0])>=count:
+#         a=0
+#         j=0
+#         for j in range(len(m2_matrix)):
+#             a+=int(m1_matrix[i][j])*int(m2_matrix[j][count-1]) 
+#         mul_1m.append(a)
 
-    return mul_1m
-def mult_matrix(m1, m2):
+#     return mul_1m
+def mult_matrix(m1_matrix, m2_matrix):
     '''
         check if the matrix1 columns = matrix2 rows
         mult the matrices and return the result matrix
@@ -22,39 +22,39 @@ def mult_matrix(m1, m2):
         and return None
         error message should be "Error: Matrix shapes invalid for mult"
     '''
-    #print(len(m1[0]),len(m2))
+    #print(len(m1_matrix[0]),len(m2_matrix))
     mul_2m=[]
     try:
-        if len(m1[0])==len(m2):
-            for i in range(len(m1[0])):
+        if len(m1_matrix[0])==len(m2_matrix):
+            for i in range(len(m1_matrix[0])):
                 mul_1m=[]
                 count=0
-               # loop(mul_1m,i,j,m1,m2,count)
-                #loop(loop(loop(loop(mul_1m,i,j,m1,m2,count),i,j,m1,m2,count),i,j,m1,m2,count),i,j,m1,m2,count)
-                # loop(mul_1m,i,j,m1,m2,count)
-                #loop(mul_1m,i,j,m1,m2,count)
-                if len(m2[0])>=1:
+               # loop(mul_1m,i,j,m1_matrix,m2_matrix,count)
+                #loop(loop(loop(loop(mul_1m,i,j,m1_matrix,m2_matrix,count),i,j,m1_matrix,m2_matrix,count),i,j,m1_matrix,m2_matrix,count),i,j,m1_matrix,m2_matrix,count)
+                # loop(mul_1m,i,j,m1_matrix,m2_matrix,count)
+                #loop(mul_1m,i,j,m1_matrix,m2_matrix,count)
+                if len(m2_matrix[0])>=1:
                     a=0
-                    for j in range(len(m2)):
-                        a+=int(m1[i][j])*int(m2[j][0])
+                    for j in range(len(m2_matrix)):
+                        a+=int(m1_matrix[i][j])*int(m2_matrix[j][0])
                     mul_1m.append(a)
 
-                if len(m2[0])>=2:
+                if len(m2_matrix[0])>=2:
                     a=0
-                    for k in range(len(m2)):
-                        a+=int(m1[i][k])*int(m2[k][1])  
+                    for k in range(len(m2_matrix)):
+                        a+=int(m1_matrix[i][k])*int(m2_matrix[k][1])  
                     mul_1m.append(a)
                 
-                if len(m2[0])>=3:
+                if len(m2_matrix[0])>=3:
                     a=0
                     count+=1
-                    for n in range(len(m2)):
-                        a+=int(m1[i][n])*int(m2[n][2])   
+                    for n in range(len(m2_matrix)):
+                        a+=int(m1_matrix[i][n])*int(m2_matrix[n][2])   
                     mul_1m.append(a)
-                if len(m2[0])>=4:
+                if len(m2_matrix[0])>=4:
                     a=0
-                    for m in range(len(m2)):
-                        a+=int(m1[i][m])*int(m2[m][3])
+                    for m in range(len(m2_matrix)):
+                        a+=int(m1_matrix[i][m])*int(m2_matrix[m][3])
                     mul_1m.append(a)
                     
 
@@ -67,7 +67,7 @@ def mult_matrix(m1, m2):
 
     return mul_2m
 
-def add_matrix(m1, m2):
+def add_matrix(m1_matrix, m2_matrix):
     '''
         check if the matrix shapes are similar
         add the matrices and return the result matrix
@@ -80,11 +80,11 @@ def add_matrix(m1, m2):
     add_2m=[]
 
     try:
-        if len(m1[1])==len(m2[0]):
-            for i in range(len(m1)):
+        if len(m1_matrix[1])==len(m2_matrix[0]):
+            for i in range(len(m1_matrix)):
                 add_m = []
-                for j in range(len(m2[0])):
-                    add_m.append(int(m1[i][j])+int(m2[i][j]))
+                for j in range(len(m2_matrix[0])):
+                    add_m.append(int(m1_matrix[i][j])+int(m2_matrix[i][j]))
             #add_m.append(add_m)
                 add_2m.append(add_m)
         else:
@@ -94,8 +94,8 @@ def add_matrix(m1, m2):
             return "Error: Invalid input for the matrix"
     
 
-        # add_m[i][1]=m1[i][1]+m2[i][1]
-        # add_m[i][2]=m1[i][2]+m2[i][2]
+        # add_m[i][1]=m1_matrix[i][1]+m2_matrix[i][1]
+        # add_m[i][2]=m1_matrix[i][2]+m2_matrix[i][2]
     
 
     return add_2m
@@ -119,25 +119,24 @@ def main():
     
 
     # read matrix 1
-    m1=read_matrix()
-    m2=read_matrix()
+    m1_matrix=read_matrix()
+    m2_matrix=read_matrix()
     # read matrix 2
-    # print(m1)
-    # print(m2)
-    # print(m1)
-    # print(m2)
-    # if add_matrix(m1, m2)==mult_matrix(m1, m2):
-    #     print(mult_matrix(m1, m2))
+    # print(m1_matrix)
+    # print(m2_matrix)
+    # print(m1_matrix)
+    # print(m2_matrix)
+    # if add_matrix(m1_matrix, m2_matrix)==mult_matrix(m1_matrix, m2_matrix):
+    #     print(mult_matrix(m1_matrix, m2_matrix))
     
     # else:
-    a=add_matrix(m1, m2)
+    a_add=add_matrix(m1_matrix, m2_matrix)
     print(a)
-    b=mult_matrix(m1,m2)
-    if a==b:
-        c=0
-        c=a
+    b_mult=mult_matrix(m1_matrix,m2_matrix)
+    if a_add==b_mult:
+        c_temp=0
     else:
-        print(b)    
+        print(b_mult)    
     # multiply matrix 1 and matrix 2
 
 if __name__ == '__main__':
