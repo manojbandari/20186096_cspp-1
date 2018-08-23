051cef520b3e72da3423af9b02fcaf49874b562c
manojbandari <bandarimanoj22@msitprogram.net>
2018-08-23 18:55:05 +0530

    " -> 0 of 8 passed. pylint: 0/0 "

--- cspp1-assignments/m20/CodeCampMatrixOperations/matrix_operations.py
+++ cspp1-assignments/m20/CodeCampMatrixOperations/matrix_operations.py
@@ -30,32 +30,32 @@ def mult_matrix(m1, m2):
                 mul_1m=[]
                 count=0
                # loop(mul_1m,i,j,m1,m2,count)
-                loop(loop(loop(loop(mul_1m,i,j,m1,m2,count),i,j,m1,m2,count),i,j,m1,m2,count),i,j,m1,m2,count)
+                #loop(loop(loop(loop(mul_1m,i,j,m1,m2,count),i,j,m1,m2,count),i,j,m1,m2,count),i,j,m1,m2,count)
                 # loop(mul_1m,i,j,m1,m2,count)
-                # loop(mul_1m,i,j,m1,m2,count)
-                # if len(m2[0])>=1
-                #     a=0
-                #     for j in range(len(m2)):
-                #         a+=int(m1[i][j])*int(m2[j][0])
-                #     mul_1m.append(a)
+                #loop(mul_1m,i,j,m1,m2,count)
+                if len(m2[0])>=1
+                    a=0
+                    for j in range(len(m2)):
+                        a+=int(m1[i][j])*int(m2[j][0])
+                    mul_1m.append(a)
 
-                # if len(m2[0])>=2:
-                #     a=0
-                #     for k in range(len(m2)):
-                #         a+=int(m1[i][k])*int(m2[k][1])  
-                #     mul_1m.append(a)
+                if len(m2[0])>=2:
+                    a=0
+                    for k in range(len(m2)):
+                        a+=int(m1[i][k])*int(m2[k][1])  
+                    mul_1m.append(a)
                 
-                # if len(m2[0])>=3:
-                #     a=0
-                #     count+=1
-                #     for n in range(len(m2)):
-                #         a+=int(m1[i][n])*int(m2[n][2])   
-                #     mul_1m.append(a)
-                # if len(m2[0])>=4:
-                #     a=0
-                #     for m in range(len(m2)):
-                #         a+=int(m1[i][m])*int(m2[m][3])
-                #     mul_1m.append(a)
+                if len(m2[0])>=3:
+                    a=0
+                    count+=1
+                    for n in range(len(m2)):
+                        a+=int(m1[i][n])*int(m2[n][2])   
+                    mul_1m.append(a)
+                if len(m2[0])>=4:
+                    a=0
+                    for m in range(len(m2)):
+                        a+=int(m1[i][m])*int(m2[m][3])
+                    mul_1m.append(a)
                     
 
                 mul_2m.append(mul_1m)
