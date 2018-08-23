00606e180481c02ff3701ae1aa5fcbf0519bbf48
manojbandari <bandarimanoj22@msitprogram.net>
2018-08-23 20:04:39 +0530

    " -> 0 of 8 passed. pylint: 0/0 "

--- cspp1-assignments/m20/CodeCampMatrixOperations/matrix_operations.py
+++ cspp1-assignments/m20/CodeCampMatrixOperations/matrix_operations.py
@@ -1,6 +1,14 @@
 '''
 @author: manojbandari
 '''
+def loop(mul_1m,i,m1_matrix,m2_matrix,count):
+    count += 1
+    if len(m2_matrix[0]) >= count:
+        a = 0
+            for j_value in range(len(m2_matrix)):
+                a += int(m1_matrix[i][j_value]) * int(m2_matrix[j_value][count-1])
+            return mul_1m.append(a)
+
 def mult_matrix(m1_matrix, m2_matrix):
     '''
         check_value if the matrix1 columns = matrix2 rows
@@ -17,32 +25,32 @@ def mult_matrix(m1_matrix, m2_matrix):
                 mul_1m = []
                 count = 0
                # loop(mul_1m,i,j_value,m1_matrix,m2_matrix,count)
-                #loop(loop(loop(loop(mul_1m,i,j_value,m1_matrix,m2_matrix,count),i,j_value,m1_matrix,m2_matrix,count),i,j_value,m1_matrix,m2_matrix,count),i,j_value,m1_matrix,m2_matrix,count)
+                loop(loop(loop(loop(mul_1m,i,m1_matrix,m2_matrix,count),i,m1_matrix,m2_matrix,count),i,m1_matrix,m2_matrix,count),i,m1_matrix,m2_matrix,count)
                 # loop(mul_1m,i,j_value,m1_matrix,m2_matrix,count)
                 #loop(mul_1m,i,j_value,m1_matrix,m2_matrix,count)
-                if len(m2_matrix[0]) >= 1:
-                    a = 0
-                    for j_value in range(len(m2_matrix)):
-                        a += int(m1_matrix[i][j_value]) * int(m2_matrix[j_value][0])
-                    mul_1m.append(a)
+                # if len(m2_matrix[0]) >= 1:
+                #     a = 0
+                #     for j_value in range(len(m2_matrix)):
+                #         a += int(m1_matrix[i][j_value]) * int(m2_matrix[j_value][0])
+                #     mul_1m.append(a)
 
 
-                if len(m2_matrix[0]) >= 2:
-                    a = 0
-                    for k_value in range(len(m2_matrix)):
-                        a += int(m1_matrix[i][k_value]) * int(m2_matrix[k_value][1])  
+                # if len(m2_matrix[0]) >= 2:
+                #     a = 0
+                #     for k_value in range(len(m2_matrix)):
+                #         a += int(m1_matrix[i][k_value]) * int(m2_matrix[k_value][1])  
         
-                    mul_1m.append(a)
-                if len(m2_matrix[0]) >= 3:
-                    a = 0
-                    for n_value in range(len(m2_matrix)):
-                        a += int(m1_matrix[i][n_value]) * int(m2_matrix[n_value][2])   
-                    mul_1m.append(a)
-                if len(m2_matrix[0]) >= 4:
-                    a=0
-                    for m_value in range(len(m2_matrix)):
-                        a += int(m1_matrix[i][m_value]) * int(m2_matrix[m_value][3])
-                    mul_1m.append(a)
+                #     mul_1m.append(a)
+                # if len(m2_matrix[0]) >= 3:
+                #     a = 0
+                #     for n_value in range(len(m2_matrix)):
+                #         a += int(m1_matrix[i][n_value]) * int(m2_matrix[n_value][2])   
+                #     mul_1m.append(a)
+                # if len(m2_matrix[0]) >= 4:
+                #     a=0
+                #     for m_value in range(len(m2_matrix)):
+                #         a += int(m1_matrix[i][m_value]) * int(m2_matrix[m_value][3])
+                #     mul_1m.append(a)
                     
 
                 mul_2m.append(mul_1m)
@@ -80,8 +88,6 @@ def add_matrix(m1_matrix, m2_matrix):
     except:
             return "Error: Invalid input for the matrix"
 
-        # add_m[i][1]=m1_matrix[i][1]+m2_matrix[i][1]
-        # add_m[i][2]=m1_matrix[i][2]+m2_matrix[i][2]
     return add_2m
 def read_matrix():
     '''
@@ -104,15 +110,6 @@ def main():
     # read matrix 1
     m1_matrix = read_matrix()
     m2_matrix = read_matrix()
-    # read matrix 2
-    # print(m1_matrix)
-    # print(m2_matrix)
-    # print(m1_matrix)
-    # print(m2_matrix)
-    # if add_matrix(m1_matrix, m2_matrix)==mult_matrix(m1_matrix, m2_matrix):
-    #     print(mult_matrix(m1_matrix, m2_matrix))
-    
-    # else:
     a_add = add_matrix(m1_matrix, m2_matrix)
     print(a_add)
     b_mult = mult_matrix(m1_matrix, m2_matrix)
