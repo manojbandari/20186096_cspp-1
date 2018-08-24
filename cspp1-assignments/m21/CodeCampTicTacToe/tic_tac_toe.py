5f907b0d3860c93172fb478df74566b599562fbc
manojbandari <bandarimanoj22@msitprogram.net>
2018-08-24 12:20:55 +0530

    " -> 9 of 14 passed. pylint: -11.63/10 "

--- cspp1-assignments/m21/CodeCampTicTacToe/tic_tac_toe.py
+++ cspp1-assignments/m21/CodeCampTicTacToe/tic_tac_toe.py
@@ -9,13 +9,15 @@ def Winnner_check(matrix):
 				count_a+=1
 			elif matrix[i][j] =='x':
 				count_b+=1
-		if count_a==count_b:
-			return 'invalid game'
+		
 		if count_a==3:
 			return 'o'
 
 		elif count_b==3:
+			if count_a==count_b:
+				return 'invalid game'
 			return 'x'
+	
 		
 	if count_a==5 and count_b==4 or count_a==4 and count_b==5:
 		return 'draw'
