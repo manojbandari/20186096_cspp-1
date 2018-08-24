1bdfd0df3ead52d8c8e233eebb40d3b161a95a65
manojbandari <bandarimanoj22@msitprogram.net>
2018-08-24 16:23:27 +0530

    " -> 14 of 14 passed. pylint: 7.40/10 "

--- cspp1-assignments/m21/CodeCampTicTacToe/tic_tac_toe.py
+++ cspp1-assignments/m21/CodeCampTicTacToe/tic_tac_toe.py
@@ -1,5 +1,11 @@
-
+'''
+@author: manoj bandari
+program for tic_tac toe game
+''' 
 def Winnner_check(matrix):
+    '''
+    return result of the game
+    '''
     a_count = 0
     b_count = 0
     for i in range(3):
@@ -13,9 +19,8 @@ def Winnner_check(matrix):
         if (a_count == 5 and b_count == 4) or (a_count == 4 and b_count == 5):
             return 'draw'
     a_count = 0
-    b_count = 0 
+    b_count = 0
     for i in range(3):
-        
         for j in range(3):
             if matrix[i][j] in 'ox.':
                 if matrix[i][j] == 'o':
@@ -43,9 +48,13 @@ def Winnner_check(matrix):
         if a_count == 3:
             return 'o'
         elif b_count == 3:
-            return 'x' 
+            return 'x'
 
 def valid_game(matrix):
+    '''
+    checking the game is valid or not
+    return boolean values
+    '''
     a_count = 0
     b_count = 0
     for i in range(3):
@@ -53,22 +62,36 @@ def valid_game(matrix):
             if matrix[i][j] in 'ox.':
                 if matrix[i][j] == 'o':
                     a_count += 1
-                elif matrix[i][j] =='x':
+                elif matrix[i][j] == 'x':
                     b_count += 1
     return bool(((a_count <= 5 and b_count <= 4) or (a_count <= 4 and b_count <= 5)) and abs(a_count - b_count) <= 1)
 
 def is_valid(matrix):
+    '''
+    checking whether the input is right or wrong
+    return boolean values
+    '''
     for i in range(3):
         for j in range(3):
             if matrix[i][j] not in 'ox.':
                 return False
     return True
 def read_input():
+    '''
+    Taking the inputs from the user for matrix
+    return matrix
+    '''
     matrix = []
     for _ in range(3):
         matrix.append(input().split(" "))
     return matrix
 def main():
+    '''
+    main function 
+    printing who is the winner 
+    invalid input if the input is wrong
+    invalid game if the input entered is wrong
+    '''
     matrix = read_input()
     if is_valid(matrix):
         if valid_game(matrix):
