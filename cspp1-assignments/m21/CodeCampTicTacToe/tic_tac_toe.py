1e7a970325151d0de101b63a7ccc31917ed47ff1
manojbandari <bandarimanoj22@msitprogram.net>
2018-08-24 12:09:21 +0530

    " -> 7 of 14 passed. pylint: -10.85/10 "

--- cspp1-assignments/m21/CodeCampTicTacToe/tic_tac_toe.py
+++ cspp1-assignments/m21/CodeCampTicTacToe/tic_tac_toe.py
@@ -9,8 +9,8 @@ def Winnner_check(matrix):
 				count_a+=1
 			elif matrix[i][j] =='x':
 				count_b+=1
-		if count_a==5 and count_b==4 or count_a==4 and count_b==5:
-			return 'draw'
+	if count_a==5 and count_b==4 or count_a==4 and count_b==5:
+		return 'draw'
 	for i in range(1):
 		for j in range(3):
 			if matrix[i][j]=='o' and matrix[i+1][j]=='o' and matrix[i+2][j]=='o':
