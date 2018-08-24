7bef237b1bf35e99f235d1b399a3cb24a4c220d5
manojbandari <bandarimanoj22@msitprogram.net>
2018-08-24 14:28:25 +0530

    " -> 4 of 14 passed. pylint: -7.91/10 "

--- cspp1-assignments/m21/CodeCampTicTacToe/tic_tac_toe.py
+++ cspp1-assignments/m21/CodeCampTicTacToe/tic_tac_toe.py
@@ -1,59 +1,44 @@
 def Winnner_check(matrix):
-	
-	for i in range(3):
-		count_a=0
-		count_b=0
-		for j in range(3):
-			
-			if matrix[i][j] == 'o':
+	for i in matrix:
+		for j in i:
+			if j == 'o':
 				count_a+=1
-			elif matrix[i][j] =='x':
+			if j =='x':
 				count_b+=1
+
 		if count_a==count_b:
-			return 'invalid game'
-		if count_a==3:
-			return 'o'
+			print(draw)
+		elif count_a==3:
+			print('o')
 
 		elif count_b==3:
-			return 'x'
+			print('x')
+
+
+def valid_game(matrix):
+	a=0
+	b=0
+	for i in range(3):
 		
-	if count_a==5 and count_b==4 or count_a==4 and count_b==5:
-		return 'draw'
-	
-	for i in range(1):
 		for j in range(3):
-			if matrix[i][j]=='o' and matrix[i+1][j]=='o' and matrix[i+2][j]=='o':
-				return 'o'
-			elif matrix[i][j]=='x' and matrix[i+1][j]=='x' and matrix[i+2][j]=='x':
-				return 'x'
-		
-		
-		
-	
+			if matrix[i][j] == 'o':
+				a+=1
+			elif matrix[i][j] =='x':
+				b+=1
 
 
+	return bool(((a<=5 and b<=4) or (a<=4 and b<=5)) and 0>=abs(a-b)<=1)
 
 
 
 def is_valid(matrix):
 	
-	a=0
-	b=0
-	c=0
 	for i in range(3):
 		for j in range(3):
-			if matrix[i][j] in 'ox.':
-				if matrix[i][j]=='o':
-					a+=1
-				elif matrix[i][j]=='x':
-					b+=1
-				else:
-					c+=1
-			else:
-				return "invalid input"
-
-
-	return bool(((a<=5 and b<=4) or (a<=4 and b<=5)) and a-b<=1)
+			if matrix[i][j] not in 'ox.':
+				return False
+	return True
+ 
 	
 def read_input():
 	matrix = []
@@ -65,10 +50,12 @@ def read_input():
 def main():
 	matrix=read_input()
 	if is_valid(matrix):
-		print(Winnner_check(matrix))
-
+		if valid_game(matrix):
+			print(Winnner_check(matrix))
+		else:
+			print("invalid game")
 	else:
-		print("invalid game")
+		print("invalid input")
 	
 
 
