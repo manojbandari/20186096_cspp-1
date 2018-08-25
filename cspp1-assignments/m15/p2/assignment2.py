3d7dd7482b89209f9f1d71b28fb42833e97bc302
manojbandari <bandarimanoj22@msitprogram.net>
2018-08-25 22:48:36 +0530

    " -> 1 of 1 passed. pylint: 9.78/10 "

--- cspp1-assignments/m15/p2/assignment2.py
+++ cspp1-assignments/m15/p2/assignment2.py
@@ -45,7 +45,7 @@ def is_word(word_list, word):
     False
     '''
     word = word.lower()
-    word = word.strip(" !@#$%^&*()-_+={}[]|\:;'<>?,./\"")
+    word = word.strip(" !@#$%^&*()-_+={}[]|:;'<>?,./\"")
     return word in word_list
 
 ### DO NOT MODIFY THIS FUNCTION ###
@@ -60,7 +60,7 @@ def get_story_string():
 
 WORDLIST_FILENAME = 'words.txt'
 
-class Message(object):
+class Message():
     ''' Message object '''
     ### DO NOT MODIFY THIS METHOD ###
     def __init__(self, text):
@@ -165,6 +165,7 @@ class PlaintextMessage(Message):
         Hint: consider using the parent class constructor so less
         code is repeated
         '''
+        Message.__init__(self, text)
         self.text = text
         self.shift = shift
         self.valid_words = load_words("words.txt")
@@ -228,6 +229,7 @@ class CiphertextMessage(Message):
             self.message_text (string, determined by input text)
             self.valid_words (list, determined using helper function load_words)
         '''
+        Message.__init__(self, text)
         self.message_text = text
         self.valid_words = load_words("words.txt")[:]
         self.max_valid_words = 0
