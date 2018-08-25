7d2f15d8c1407662dc28cc851410bf3d6ea148d0
manojbandari <bandarimanoj22@msitprogram.net>
2018-08-25 13:29:04 +0530

    " -> 22 of 26 passed. pylint: 9.50/10 "

--- cspp1-assignments/m22/Check Sudoku/Check Sudoku/check_sudoku.py
+++ cspp1-assignments/m22/Check Sudoku/Check Sudoku/check_sudoku.py
@@ -19,7 +19,7 @@ def check_sudoku(sudoku):
         for j in range(len(sudoku[0])):
             count_val = set(sudoku[i])
             lis.append(sudoku[j][i])
-            #if sudoku[i][0] == sudoku 
+            #if sudoku[i][0] == sudoku
         if count_val != len(sudoku):
             return False
         check = set(lis)
