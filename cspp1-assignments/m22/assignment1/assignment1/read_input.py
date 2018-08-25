3367ddf6dfce280801a4eb5704662e7696629cdb
manojbandari <bandarimanoj22@msitprogram.net>
2018-08-25 11:11:16 +0530

    " -> 0 of 2 passed. pylint: 5.83/10 "

--- cspp1-assignments/m22/assignment1/assignment1/read_input.py
+++ cspp1-assignments/m22/assignment1/assignment1/read_input.py
@@ -7,14 +7,14 @@ def main():
     '''
     read multiple lines
     '''
-    number_of_lines=int(input())
-    multiple_lines=" "
-    lis=[]
+    number_of_lines = int(input())
+    multiple_lines = " "
+    lis_output = []
     for i in range(number_of_lines):
-        multiple_lines+=input()
-        multiple_lines=multiple_lines+"0@"
-    lis=multiple_lines.split("0@")
-    for i in range(len(lis)):
+        multiple_lines += input()
+        multiple_lines = multiple_lines+"0@"
+    lis_output = multiple_lines.split("0@")
+    for i in enumerate(lis_output):
         print(lis[i].strip())
 
 
