83f4920cbbc441c446db01574070e8f28ea998bc
manojbandari <bandarimanoj22@msitprogram.net>
2018-08-25 16:00:06 +0530

    " -> 21 of 26 passed. pylint: 9.50/10 "

--- cspp1-assignments/m22/Check Sudoku/Check Sudoku/check_sudoku.py
+++ cspp1-assignments/m22/Check Sudoku/Check Sudoku/check_sudoku.py
@@ -25,8 +25,6 @@ def check_sudoku(sudoku):
         check = set(lis)
         if len(check) != len(sudoku):
             return False
-        if sudoku[i][j] == sudoku[i][i]:
-            return False
 
     return True
 def main():
