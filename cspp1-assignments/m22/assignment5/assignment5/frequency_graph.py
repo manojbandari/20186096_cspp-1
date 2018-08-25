a7e3979271c2d5e26ba96e176e3378917d29cf51
manojbandari <bandarimanoj22@msitprogram.net>
2018-08-25 12:23:21 +0530

    " -> 4 of 4 passed. pylint: 8.18/10 "

--- cspp1-assignments/m22/assignment5/assignment5/frequency_graph.py
+++ cspp1-assignments/m22/assignment5/assignment5/frequency_graph.py
@@ -7,13 +7,16 @@ def frequency_graph(dictionary):
     '''
     frequency graph function
     '''
-     for key in sorted(dictionary):
-        for i in range(11):
+    for key in sorted(dictionary):
+        for i in range(20):
             if dictionary[key]==i:
                 dictionary[key] = i*'#'
         print(key, "-", dictionary[key])
 
 def main():
+    '''
+    main function
+    '''
     dictionary = eval(input())
     frequency_graph(dictionary)
 
